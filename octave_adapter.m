3c66fb10a41252bab8c1e1b6354ca337b38a707c
FluxTape <fluxtape.contact@gmail.com>
2024-06-14 16:15:38 +0200

    wire up stdin/stdout with octave

--- octave_adapter.m
+++ octave_adapter.m
@@ -1,14 +1,21 @@
 data_str   = fread( stdin, 'char' );
 data_str   = char( data_str.' );
-str_tokens = strsplit( data_str ); 
+str_tokens = strsplit( data_str )
 tokens     = cellfun( @str2double, str_tokens );
+tokens = tokens(1:end-1);
+err_at = find(isnan(tokens));
+if !isempty(err_at)
+    disp("failed to parse input at index:");
+    err_at
+    return
+endif
 w_start              = tokens(1)
 w_end                = tokens(2)
 w_points_internal    = round(tokens(3))
 order                = round(tokens(4))
 divs_search_grid     = round(tokens(5))
 includes_err_weights = round(tokens(6))
-data_p = tokens(7:end-1)
+data_p = tokens(7:end)
 if (includes_err_weights > 0)
     w_points = numel(data_p)/2
     gradient_ref = data_p(1:w_points)
