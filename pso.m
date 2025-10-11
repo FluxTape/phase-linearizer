319942be12704340bb873de51188e593c7b09993
FluxTape <fluxtape.contact@gmail.com>
2025-10-11 16:26:31 +0200

    pso changes for testing

--- pso.m
+++ pso.m
@@ -9,7 +9,7 @@ function [ret, ret_start, ret_best_costs] = pso(cf, nr_variables, var_min, var_m
   
     %% Parameter Adjustment
     swarm_size = 700;                       % Swarm size (number of particles)
-    c1 = 0.95;                               % Inertia coefficient
+    c1 = 1.0;                               % Inertia coefficient
     w = 1;                                 % initally no damping: w=1
     w_damp = 0.99;                          % damping of inertia coefficient, lower = faster damping
     c2 = 1.43;                              % Cognitive acceleration coefficient (c1 + c2 = 4)
@@ -92,10 +92,12 @@ function [ret, ret_start, ret_best_costs] = pso(cf, nr_variables, var_min, var_m
         r2 = rand(variable_size);
         r3 = rand(variable_size);
   
-        % Update velocity %
-        particles(i).velocity = (c1 * w * (r1 * 0.5 + 0.75) .* particles(i).velocity) ...
+        % Update velocity %* (r1 * 0.5 + 0.75)
+        particles(i).velocity = (c1 * w  .* particles(i).velocity) ...
           + (c1 * r2 .* (particles(i).best.position - particles(i).position)) ...
           + (c2 * r3 .* (global_best.position - particles(i).position));
+
+        % TODO limit velocity (scale each dimension by the same factor to preserve direction)
   
         % Update position
         particles(i).position = particles(i).position + particles(i).velocity;
