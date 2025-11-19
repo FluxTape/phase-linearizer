5d02c204af47337c972c3efe15cdca3c36d74dbb
FluxTape <fluxtape.contact@gmail.com>
2025-11-19 12:04:02 +0100

    update to test functions

--- test_functions.m
+++ test_functions.m
@@ -23,17 +23,13 @@ chebyBP = tf(b3, a3, pi)
 % peak dip 
 b4 = [1.0349   -0.9159    0.7704   -0.4199    0.1551]
 a4 = [1.0000   -0.9882    0.7910   -0.3476    0.1694]
-peakDip = tf(b4, a4, pi)
+peakDip = tf(b4, a4, pi) ^2
+[b4_1, a4_1] = tfdata(peakDip, 'v')
 
 % butter bandstop
 [b5_1, a5_1] = butter(4, [0.1, 0.5], 'stop')
 h5_1 = tf(b5_1, a5_1, pi)
-%[b5_2, a5_2] = butter(3, 0.6)
-%h5_2 = tf(b5_2, a5_2, pi)
-%butterStopLP = h5_1 * h5_2
-%[b5, a5] = tfdata(butterStopLP, 'v')
-
-[b5_2, a5_2] = bilinear([0 0 0.25],[1 0.0636 0.25], pi)
+[b5_2, a5_2] = bilinear([0 0 0.25],[1 0.07 0.25], pi)
 h5_2 = tf(b5_2, a5_2, pi)
 butterStopLP = h5_1 * h5_2
 [b5, a5] = tfdata(butterStopLP, 'v')
@@ -42,9 +38,9 @@ bode(chebyLP, chebyHP, chebyBP, peakDip, butterStopLP, wb)
 subplot(2,1,1)
 title('Bode diagram of all test functions')
 ylim([-100 10])
-h = legend ('Cheby LP', 'Cheby HP', 'Cheby BP', 'Peak & Dip', 'Butterworth Stop & LP                   ', "location", "northwest");
+h = legend ('Cheby LP', 'Cheby HP', 'Cheby BP', 'Peak & Dip', 'Stop & LP                   ', "location", "northwest");
 legend (h, "location", "northwest")
 
 subplot(2,1,2)
-h = legend ('Cheby LP', 'Cheby HP', 'Cheby BP', 'Peak & Dip', 'Butterworth Stop & LP                   ', "location", "northwest");
+h = legend ('Cheby LP', 'Cheby HP', 'Cheby BP', 'Peak & Dip', 'Stop & LP                   ', "location", "northwest");
 legend (h, "location", "northwest")
