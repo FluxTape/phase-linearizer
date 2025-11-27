fc3d1cff74de80c1a85b6830e27886aa5e80b06a
FluxTape <fluxtape.contact@gmail.com>
2025-11-27 12:25:37 +0100

    random-unc cheby lp data

--- experimental_results/random-unc/runc_timing_cheby_lp.sh
+++ experimental_results/random-unc/runc_timing_cheby_lp.sh
@@ -4,5 +4,6 @@ cd ../../
 set -eux
 cargo build --release
 echo "random-unc - Cheby LP"
-hyperfine --warmup 1 --runs 10 --export-csv ./experimental_results/random_unc/cheby_lp_timing.csv './target/release/phase-linearizer -n 0.0 -x 0.6 -p 150 -o 6 -a random-unc -i 300 -f ./experimental_results/random-unc/cheby_lp.csv transfer-function --amp --num "2.3447e-03   5.8262e-03   1.0160e-02   1.1842e-02   1.0160e-02   5.8262e-03   2.3447e-03" --den "1.000000  -3.207647 4.689690  -3.853772   1.857272  -0.493137   0.056098"'
-
+CMD='./target/release/phase-linearizer -n 0.0 -x 0.6 -p 150 -o 6 -a random-unc -i 300 -f ./experimental_results/random-unc/cheby_lp.csv transfer-function --amp --num "2.3447e-03   5.8262e-03   1.0160e-02   1.1842e-02   1.0160e-02   5.8262e-03   2.3447e-03" --den "1.000000  -3.207647 4.689690  -3.853772   1.857272  -0.493137   0.056098"'
+hyperfine --warmup 1 --runs 10 --export-csv ./experimental_results/random-unc/cheby_lp_timing.csv "$CMD"
+seq 89 | parallel -N0 sh -c \'"$CMD"\'
